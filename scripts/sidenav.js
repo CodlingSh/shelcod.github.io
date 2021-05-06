c728bd49f160e711b2fe0ef50cc038c436393d58
shelcod <shelcod@gmail.com>
2021-05-06 15:36:31 -0400

    Delete scripts directory

--- scripts/sidenav.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySideNav").style.width = "100%";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySideNav").style.width = "0";
-}
