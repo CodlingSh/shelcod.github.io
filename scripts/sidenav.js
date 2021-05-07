ca63fcf56cbb6d5cc133a165ae64a7055b9bbe75
shelcod <shelcod@gmail.com>
2021-05-07 16:47:54 -0400

    Add files via upload

---
+++ scripts/sidenav.js
@@ -0,0 +1,21 @@
+// Set the width of the side navigation to 250px and the left margin of the page content to 250px
+/*function openNav() {
+    document.getElementById("mySideNav").style.width = "100%";
+}
+
+// Set the width of the side navigation to 0 and the left margin of the page content to 0
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+}*/
+
+//Jquery test
+jQuery(document).ready(function() {
+    
+    jQuery(".menuBtn").click(function() {
+        jQuery("#mySideNav").toggleClass("hide");
+    });
+
+    jQuery(".pixelPortrait").click(function() {
+        
+    });
+});
