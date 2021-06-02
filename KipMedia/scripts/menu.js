545debf534d1e3a4b08d4118a6f80c8d3179a02d
Sheldon Codling <shelcod@gmail.com>
2021-06-02 01:24:59 -0400

    Add files via upload

--- KipMedia/scripts/menu.js
+++ KipMedia/scripts/menu.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+    //$('.menu').click(function() {
+    //    $('.dropdown').toggleClass("active")
+    //})
 
+    $('.menu').click(function() {
+        
+        if ($('.dropdown').css("display") == "block") {
+            $(".dropdown").slideUp(200, "swing");
+            $(".arrow").css("transform", "rotate(0deg)");
+        }
+        else {
+            $(".dropdown").slideDown(200, "swing");
+            $(".dropdown").css("display", "block");
+            $(".arrow").css("transform", "rotate(180deg)");
+        }
+    })
+})
